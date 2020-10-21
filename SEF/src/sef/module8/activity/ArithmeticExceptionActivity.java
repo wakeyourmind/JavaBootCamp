3ae3fabcba438e5c4a0599869c589d1454d010aa
Dmitrijs Lvovs <maser123@inbox.lv>
2020-10-21 12:39:38 +0300

    ArithmeticExceptionActivity complete

--- SEF/src/sef/module8/activity/ArithmeticExceptionActivity.java
+++ SEF/src/sef/module8/activity/ArithmeticExceptionActivity.java
@@ -2,7 +2,10 @@ package sef.module8.activity;
 // Needs to be completed
 public class ArithmeticExceptionActivity {
 	//1 - Type main method and call catchMe with 10 and 0
-
+	public static void main(String arg[]){
+	ArithmeticExceptionActivity ccc = new ArithmeticExceptionActivity();
+	ccc.catchMe(10, 0);
+	}
 
 void catchMe(int num1, int num2)
 {
@@ -10,8 +13,20 @@ void catchMe(int num1, int num2)
 	//2 - Identify the exception and write code to handle this exception.
 
 	int result=num1/num2;
-	System.out.println("The result is :" + result);
+	//System.out.println("The result is :" + result);
 	
+	try {
+		System.out.println("The result is :" + result);
+	} 
+	catch (NullPointerException e) {
+		System.out.println("A variable is not initialized");
+	}
+	catch(ArithmeticException e) {
+		System.out.println("I can not perform division operation if divident is zero");
+	}
+	finally {
+		System.out.println("Thank you for using this program! ");
+	}
 	//3 - After handling the exception, write a finally block which
 	//prints a message "Thank you for using this program."
 	
@@ -20,3 +35,4 @@ void catchMe(int num1, int num2)
 }
 
 }
+
